e758ee7c0f500385e4b207528a80eeb7d9fbf889
Konyhási Máté <konyhasim21@ady-nagyatad.sulinet.hu>
2024-09-10 13:26:23 +0200

    bla

---
+++ asztali/szo generator/gyak/gyak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args, Random random1)
+        {
+            List<string> list = new List<string>();
+            StreamReader nevek = new StreamReader("nevek.txt");
+
+
+            while (!nevek.EndOfStream) {
+                list.Add(nevek.ReadLine());
+            }
+            nevek.Close();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            
+        }
+        
+    }
+}
