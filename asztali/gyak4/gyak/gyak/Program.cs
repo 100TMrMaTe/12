4439a201b534f524af506ccc7c31ab3a3b69f10d
Konyhási Máté <konyhasim21@ady-nagyatad.sulinet.hu>
2024-09-13 14:17:49 +0200

    bla

---
+++ asztali/gyak4/gyak/gyak/Program.cs
@@ -0,0 +1,48 @@
+﻿static int szamBekeres(int minimum = 3, int maximum = 15)
+{
+    if (minimum > maximum)
+    {
+        throw new ArgumentException("A minimum nem lehet nagyobb mint a maximum ");
+    }
+    int szam = 0;
+    while (szam == 0)
+    {
+        try
+        {
+            Console.Write("Kérek egy egész számot {0} és {1} között: ", minimum, maximum);
+            szam = Convert.ToInt32(Console.ReadLine());
+            if (szam < minimum || szam > maximum)
+            {
+                throw new Exception();
+            }
+        }
+        catch
+        {
+            szam = 0;
+
+        }
+    }
+
+
+
+
+    return szam;
+}
+
+static string szoGeneralas(int hossz)
+{
+    string[] betuk = { "euioöüóőúűáéaí", "qwrtzpsdfghjklyxcvbnm" };
+    Random random = new Random();
+    int melyikJon = random.Next(2);
+    string vissza = "";
+    for (int i = 0; i < hossz; i++)
+    {
+        vissza += betuk[melyikJon][random.Next(betuk[melyikJon].Length)];
+    }
+
+    return vissza;
+}
+static void Main(string[] args)
+{
+    szamBekeres(5, 10);
+}
---
+++ asztali/objekt/ConsoleApp1/ConsoleApp1/kutya.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class kutya
+    {
+        public string nev;
+        public void ugat() 
+        {
+            Console.WriteLine("wau");
+        }
+        private float _suly;
+        public float suly
+        {
+            get
+            {
+                return _suly;
+            }
+            set 
+            {
+                _suly = Math.Abs(value);
+            }
+        }
+    }
+}
