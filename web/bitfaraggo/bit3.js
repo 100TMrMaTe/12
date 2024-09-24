cb9ad70722b77c46238c9b9d98c5916a5d1f8a6d
Konyhási Máté <konyhasim21@ady-nagyatad.sulinet.hu>
2024-09-24 09:40:11 +0200

    vla

--- web/bitfaraggo/bit3.js
+++ web/bitfaraggo/bit3.js
@@ -18,3 +18,41 @@ function checkTime(i)
             };
             return i;
         }
+
+        
+let szam= 0;
+function valtozas()
+{
+
+    const image = document.getElementById("kep");
+    if(szam==0)
+    {
+        image.src= "kep1.jpg";
+        szam++;
+    }
+    else if(szam==1)
+    {
+        image.src= "kep2.jpg";
+        szam++;
+    }
+    else if(szam==2)
+    {
+        image.src= "kep3.jpg";
+        szam++;
+    }
+    else if(szam==3)
+    {
+        image.src= "kep4.jpg";
+        szam++;
+    }
+    else if(szam==4)
+    {
+        image.src= "kep5.jpg";
+        szam++;
+    }
+    else if(szam==5)
+    {
+        image.src= "sr1.jpg";
+        szam++;
+    }
+}
