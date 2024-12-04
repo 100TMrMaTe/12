0e6371b7b5621dd53bf1baa0d1196116164cff4b
Konyhási Máté <konyhasim21@ady-nagyatad.sulinet.hu>
2024-12-04 12:34:29 +0100

    bla

---
+++ asztali/elso/elso/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("stream.txt"))
+            {
+                int a = 1;
+                while (a != 0)
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    writer.Write(a+" ");
+                    writer.WriteLine(a*a);
+
+                }
+            }
+            
+        }
+    }
+}
--- projekt/aula/js.js
+++ projekt/aula/js.js
@@ -1,47 +1,53 @@
-function tablazat(){
-    dragElement(document.createElement("table"));
+function tablazat() {
+  let sor = document.getElementById("sor").value;
+  let oszlop = document.getElementById("oszlop").value;
+  let table = document.createElement("table");
+  table.setAttribute("id", "table");
+
+  // Create table rows and columns
+  for (let i = 0; i < sor; i++) {
+    let tr = document.createElement("tr");
+    for (let j = 0; j < oszlop; j++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+
+  // Make the table draggable
+  makeTableDraggable(table);
+
+  // Append table to the page
+  document.getElementById("left").appendChild(table);
+}
+
+function makeTableDraggable(table) {
+  let isDragging = false;
+  let offsetX, offsetY;
+
+  table.style.position = 'absolute'; // Make sure the table is positioned absolutely
+
+  // Mouse down event to start dragging
+  table.addEventListener('mousedown', function (e) {
+    isDragging = true;
+    offsetX = e.clientX - table.offsetLeft;
+    offsetY = e.clientY - table.offsetTop;
+    table.style.cursor = 'move';
+  });
+
+  // Mouse move event to move the table
+  document.addEventListener('mousemove', function (e) {
+    if (isDragging) {
+      table.style.left = e.clientX - offsetX + 'px';
+      table.style.top = e.clientY - offsetY + 'px';
+    }
+  });
+
+  // Mouse up event to stop dragging
+  document.addEventListener('mouseup', function () {
+    isDragging = false;
+    table.style.cursor = 'default';
+  });
 }
 
 
-
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
